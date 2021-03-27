d127f2d119d821ac7a4667913d722349a8d894c0
hexilee <i@hexilee.me>
2021-03-27 23:30:27 +0800

    init tracing_subscriber
    Signed-off-by: hexilee <i@hexilee.me>

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,19 @@ pub mod tproxy;
 
 use std::net::SocketAddr;
 
+use anyhow::anyhow;
 use cmd::get_config;
 use hyper::Server;
 use tproxy::{HttpServer, TcpIncoming};
+use tracing_subscriber::EnvFilter;
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_env_filter(EnvFilter::from_default_env())
+        .try_init()
+        .map_err(|err| anyhow!("{}", err))?;
+
     let cfg = get_config();
     let addr = SocketAddr::from(([0, 0, 0, 0], cfg.tproxy_config.port));
     let incoming = TcpIncoming::bind(addr, cfg.tproxy_config.mark)?;
