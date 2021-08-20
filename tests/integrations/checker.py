958828043af22ea58020d8a4cae01c71c8e703c5
andrewmatilde <davis6813585853062@outlook.com>
2021-08-20 11:37:12 +0800

    add some integration tests
    Signed-off-by: andrewmatilde <davis6813585853062@outlook.com>

---
+++ tests/integrations/checker.py
@@ -0,0 +1,49 @@
+import http
+from http.server import BaseHTTPRequestHandler
+import json
+
+
+class HttpHandler(BaseHTTPRequestHandler):
+    check_headers = dict()
+    check_body = bytes()
+
+    def do_POST(self):
+        if "check-header" in self.path:
+            if False in [key not in self.check_headers or
+                         self.headers[key] == self.check_headers[key] for key in self.headers.keys()]:
+                self.send_response(http.HTTPStatus.BAD_REQUEST)
+            else:
+                self.send_response(http.HTTPStatus.OK)
+        elif "check-body" in self.path:
+            if self.headers["content-length"] is not None:
+                data = self.rfile.read(int(self.headers["content-length"]))
+                if data == self.check_body:
+                    self.send_response(http.HTTPStatus.BAD_REQUEST)
+                else:
+                    self.send_response(http.HTTPStatus.OK)
+            else:
+                self.send_response(http.HTTPStatus.OK)
+        elif "set-header" in self.path:
+            if self.headers["content-length"] is not None:
+                data = self.rfile.read(int(self.headers["content-length"]))
+                self.check_headers = json.loads(data)
+                self.send_response(http.HTTPStatus.OK)
+            else:
+                self.send_response(http.HTTPStatus.BAD_REQUEST)
+        elif "set-body" in self.path:
+            if self.headers["content-length"] is not None:
+                data = self.rfile.read(int(self.headers["content-length"]))
+                self.check_body = json.loads(data)
+                self.send_response(http.HTTPStatus.OK)
+            else:
+                self.send_response(http.HTTPStatus.BAD_REQUEST)
+        else:
+            self.send_response(http.HTTPStatus.BAD_REQUEST)
+        self.end_headers()
+
+
+if __name__ == '__main__':
+    from http.server import HTTPServer
+
+    server = HTTPServer(('localhost', 8080), HttpHandler)
+    server.serve_forever()
--- tests/integrations/mod.rs
+++ tests/integrations/mod.rs
@@ -1,1 +1,2 @@
-mod uds_transfer;
+mod test_uds;
+mod test_http_action;
