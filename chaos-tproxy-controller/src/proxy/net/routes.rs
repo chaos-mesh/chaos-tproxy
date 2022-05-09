08acca1bce418d937ab095e2f80972c9c28ab701
andrewmatilde <davis6813585853062@outlook.com>
2022-05-09 13:43:16 +0800

    fix comment
    Signed-off-by: andrewmatilde <davis6813585853062@outlook.com>

--- chaos-tproxy-controller/src/proxy/net/routes.rs
+++ chaos-tproxy-controller/src/proxy/net/routes.rs
@@ -6,9 +6,10 @@ use rtnetlink::{new_connection, IpVersion};
 
 pub async fn get_routes_noblock() -> Result<Vec<RouteMessage>> {
     let (connection, handle, _) = new_connection().unwrap();
-    tokio::spawn(connection);
+    let spawn = tokio::spawn(connection);
     let routes = get_routes(&handle, IpVersion::V4).await?;
 
+    spawn.abort();
     Ok(routes
         .into_iter()
         .filter(|route| route.header.table != 255)
@@ -17,8 +18,9 @@ pub async fn get_routes_noblock() -> Result<Vec<RouteMessage>> {
 
 pub async fn del_routes_noblock(msgs: Vec<RouteMessage>) -> Result<()> {
     let (connection, handle, _) = new_connection().unwrap();
-    tokio::spawn(connection);
+    let spawn = tokio::spawn(connection);
     let results = join_all(msgs.into_iter().map(|msg| del_routes(&handle, msg))).await;
+    spawn.abort();
     match results
         .into_iter()
         .filter(|result| result.is_err())
@@ -32,7 +34,7 @@ pub async fn del_routes_noblock(msgs: Vec<RouteMessage>) -> Result<()> {
 
 pub async fn load_routes(msgs: Vec<RouteMessage>) -> Result<()> {
     let (connection, mut handle, _) = new_connection().unwrap();
-    tokio::spawn(connection);
+    let spawn = tokio::spawn(connection);
 
     for route in msgs {
         IPRoute {
@@ -45,6 +47,7 @@ pub async fn load_routes(msgs: Vec<RouteMessage>) -> Result<()> {
             tracing::error!("can not recover ROUTE MSG: {:?}, error: {}", route, e)
         });
     }
+    spawn.abort();
     Ok(())
 }
 
