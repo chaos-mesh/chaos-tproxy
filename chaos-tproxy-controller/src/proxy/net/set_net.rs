6c496e48917cdde517e79eb26f8c10ef0f285ee8
Andrewmatilde <davis6813585853062@outlook.com>
2022-06-29 15:25:11 +0800

    try fix net bug (#51)
    Signed-off-by: andrewmatilde <davis6813585853062@outlook.com>

--- chaos-tproxy-controller/src/proxy/net/set_net.rs
+++ chaos-tproxy-controller/src/proxy/net/set_net.rs
@@ -1,9 +1,13 @@
 use std::option::Option::Some;
 
+use anyhow::anyhow;
+use libarp::interfaces::Interface;
 use rtnetlink::Handle;
 
-use crate::proxy::net::bridge::{bash_c, execute, execute_all, get_interface, NetEnv};
+use crate::proxy::net::arp::gratuitous_arp;
+use crate::proxy::net::bridge::{bash_c, execute, execute_all, NetEnv};
 use crate::proxy::net::iptables::{set_iptables, set_iptables_safe};
+use crate::proxy::net::ping::try_ping;
 
 #[cfg(target_os = "linux")]
 pub async fn set_net(
@@ -16,19 +20,28 @@ pub async fn set_net(
     net_env.setenv_bridge(handle).await?;
     let port = listen_port.to_string();
     let restore_dns = "cp /etc/resolv.conf.bak /etc/resolv.conf";
-    let device_interface = get_interface(net_env.veth4.clone()).unwrap();
-    let device_mac = device_interface.mac.unwrap().to_string();
+
+    let arp_interface = Interface::new_by_name(net_env.veth4.clone().as_str()).unwrap();
+    gratuitous_arp(
+        arp_interface.clone(),
+        arp_interface.get_ip().unwrap(),
+        arp_interface.get_mac(),
+    );
 
     if let Some(ref proxy_ports) = proxy_ports {
-        execute_all(set_iptables(net_env, Some(proxy_ports), &port, &device_mac))?;
+        execute_all(set_iptables(net_env, Some(proxy_ports), &port))?;
     } else {
-        execute_all(set_iptables(net_env, None, &port, &device_mac))?;
+        execute_all(set_iptables(net_env, None, &port))?;
     }
 
     if safe {
-        execute_all(set_iptables_safe(net_env, &device_mac))?;
+        execute_all(set_iptables_safe(net_env))?;
     }
     let _ = execute(bash_c(restore_dns));
+
+    let gateway = default_net::get_default_gateway().map_err(|e| anyhow!(e))?;
+    try_ping(gateway.ip_addr).await;
+
     Ok(())
 }
 
