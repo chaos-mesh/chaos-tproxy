d2aacf8e033ddef157c40efedd2ccbccc0b8ba1b
Zhiqiang ZHOU <im@strrl.dev>
2024-11-15 07:02:56 -0800

    docs: docs for UDS data client/server (#57)
    Signed-off-by: STRRL <im@strrl.dev>

--- chaos-tproxy-proxy/src/uds_client.rs
+++ chaos-tproxy-proxy/src/uds_client.rs
@@ -3,6 +3,11 @@ use std::path::PathBuf;
 use tokio::io::AsyncReadExt;
 use tokio::net::UnixStream;
 
+/// UdsDataClient is designed **ONLY** for communicate between chaos-tproxy main process and chaos-tproxy child process.
+/// It's not a general purpose Unix Domain Socket client.
+///
+/// UdsDataClient would create a connection to a certain Unix Domain Socket, and read the serialized data from that
+/// socket, then try to deserialize data to the required type.
 #[derive(Debug, Clone)]
 pub struct UdsDataClient {
     pub path: PathBuf,
@@ -13,6 +18,10 @@ impl UdsDataClient {
         Self { path }
     }
 
+    /// read_into would create a new connection to the target Unix Domain Socket and receive the serialized data from
+    /// server-side, then try to deserialize data to the required type.
+    ///
+    /// It would establish a new connection every time, so it's safe to call this method multiple times.
     pub async fn read_into<'a, T: serde::de::Deserialize<'a>>(
         &self,
         buf: &'a mut Vec<u8>,
